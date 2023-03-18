d9c51f4dd141e6b103043819253950054596b026
Julien Renault <jurenaul@student.42lausanne.ch>
2023-03-18 03:15:15 +0100

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,13 @@ import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
 import { getPosts } from '../services';
 
-export default function Home({ posts } : any) {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post : any, index : number) => (
+          {posts.map((post, index) => (
             <PostCard key={index} post={post.node} />
           ))}
         </div>
